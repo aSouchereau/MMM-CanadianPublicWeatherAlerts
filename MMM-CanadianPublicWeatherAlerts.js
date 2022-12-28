6d6ed4d6a5a894ec0e91587932d2cc6eb9a9c974
alexsouchereau <W0632604@myscc.ca>
2022-12-27 20:23:25 -0500

    Added new config defaults

--- MMM-CanadianPublicWeatherAlerts.js
+++ MMM-CanadianPublicWeatherAlerts.js
@@ -4,7 +4,7 @@
  * By Alex Souchereau http://github.com/aSouchereau
  * MIT Licensed.
  */
-Module.register('MMM-AirQuality', {
+Module.register('MMM-CanadianPublicWeatherAlerts', {
     defaults: {
             lang: 'en',
             regions: [
@@ -12,20 +12,50 @@ Module.register('MMM-AirQuality', {
                     code: ""
                 }
             ],
-            provinces: [
-                {
-                    pcode: "",
-                }
-            ],
             colour: true,
             timeSince: true,
-            updateInterval: 60 // once every minute
+            updateInterval: 60000, // once every minute (ms)
+            animationSpeed: 1000, // one second (ms)
+            displayInterval: 5000, // displays each alert for 5 seconds
+
+            apiBase: 'https://weather.gc.ca/rss/battleboard/',
+            ALERTS_PATH: '/alerts.json',
+
+    },
+
+    getStyles() {
+        return ["MMM-CanadianPublicWeatherAlerts.css"];
     },
     start: function () {
         Log.log("Starting module: " + this.name);
-        if (this.data.classes === "MMM-CanadianPublicWeatherAlerts") {
-            this.data.classes = "bright medium";
-        }
 
+        this.sendSocketNotification('CONFIG', this.config);
+        this.loaded = false;
+    },
+    getDom: function () {
+        let wrapper = document.createElement("div");
+        let innerElem = document.createElement("div");
+        if (!this.loaded) {
+            innerElem.innerHTML = "LOADING";
+        }
+        else {
+            innerElem.innerHTML = this.displayAlerts();
+        }
+        wrapper.appendChild(innerElem);
+        return wrapper;
+    },
+    displayAlerts: function (urls) {
+        // ajax call to get data from alerts.json and saves into current alerts array
+
+
+    },
+
+    socketNotificationReceived: function(notification) {
+        if (notification === "STARTED") {
+            this.updateDom();
+        } else if (notification === "UPDATE") {
+            this.loaded = true;
+            this.updateDom();
+        }
     }
 });
