79d0d3a6ef335ddfbd9e1fa01353ad3da9894bd3
alexsouchereau <W0632604@myscc.ca>
2022-12-27 21:03:17 -0500

    Added method to call update method every configured interval

--- node_helper.js
+++ node_helper.js
@@ -18,11 +18,24 @@ module.exports = NodeHelper.create({
         this.config = null;
         console.log("Starting node helper for: " + this.name);
     },
+    // Calls methods required for updating alert data
+    scheduleUpdate(delay) {
+        // Generate new urls after startup
+        let urls = generateUrls(this.config.regions);
+        // Every specified interval, fetch new data from each url
+        setInterval(() => {
+            for (let i = 0; urls.length > i; i++) {
+                let url = urls[i];
+                this.getData(url);
+            }
+        }, delay);
+    },
     socketNotificationReceived(notification, payload) {
         let self = this;
         if (notification === 'CONFIG' && self.started == false) {
             self.config = payload;
             self.sendSocketNotification("STARTED", true);
+            self.scheduleUpdate(self.config.updateInterval);
             self.started = true;
         }
     }
