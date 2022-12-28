1e2bcbcef78891ef80467189f9782f07176bc9fe
alexsouchereau <W0632604@myscc.ca>
2022-12-27 20:46:49 -0500

    Added socket notification method to receive config from module

--- node_helper.js
+++ node_helper.js
@@ -14,6 +14,16 @@ const moment = require('moment');
 
 module.exports = NodeHelper.create({
     start() {
+        this.started = false;
+        this.config = null;
         console.log("Starting node helper for: " + this.name);
+    },
+    socketNotificationReceived(notification, payload) {
+        let self = this;
+        if (notification === 'CONFIG' && self.started == false) {
+            self.config = payload;
+            self.sendSocketNotification("STARTED", true);
+            self.started = true;
+        }
     }
 });
