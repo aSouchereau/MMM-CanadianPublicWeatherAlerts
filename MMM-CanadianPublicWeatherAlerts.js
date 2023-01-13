2e6594c4a58f58b41c867ce1dfdb644e2a49b6cf
alexsouchereau <W0632604@myscc.ca>
2023-01-13 14:43:17 -0500

    Fixed last alert remaining on screen when alerts are cleared

--- MMM-CanadianPublicWeatherAlerts.js
+++ MMM-CanadianPublicWeatherAlerts.js
@@ -35,9 +35,10 @@ Module.register('MMM-CanadianPublicWeatherAlerts', {
         this.sendSocketNotification('CONFIG', this.config); // Sends config to node helper, so node helper can produce initial data
         this.scheduleUpdate(this.config.updateInterval);
     },
+    // Sends update request to server every configured interval
     scheduleUpdate(delay) {
-        this.sendSocketNotification('REQUEST_UPDATE', true); // Sends config notification on initial load
-        setInterval( () => { this.sendSocketNotification('REQUEST_UPDATE', true) }, delay); // sends update notification after each interval
+        this.sendSocketNotification('REQUEST_UPDATE', true); // Sends on initial load
+        setInterval( () => { this.sendSocketNotification('REQUEST_UPDATE', true) }, delay); // sends update request
     },
     getDom() {
         let wrapper = document.createElement("div");
@@ -51,6 +52,7 @@ Module.register('MMM-CanadianPublicWeatherAlerts', {
         wrapper.appendChild(innerElem);
         return wrapper;
     },
+    // Sets element variables to the current alert being displayed
     displayAlerts() {
         let alert = this.currentAlerts[this.currentAlertID];
         let title = alert['title'][0].split(", ");
@@ -59,16 +61,17 @@ Module.register('MMM-CanadianPublicWeatherAlerts', {
         this.AlertTime = `<div class="${this.name} alert-time">Issued ${moment(alert['updated'][0], "YYYY-MM-DDTHH:mm:ssZ").fromNow()}</div>`
         // Check to see if were at the last alert
         if (this.currentAlertID === this.currentAlerts.length - 1) {
-            this.startDisplayTimer();
+            this.startDisplayTimer(); // Restart Timer
+
         } else {
             this.currentAlertID++;
         }
     },
     startDisplayTimer() {
-        this.currentAlertID = 0;
-        clearInterval(this.timer);
+        this.currentAlertID = 0; // Makes sure we start from first index
+        clearInterval(this.timer); // Removed old timer
         this.timer = setInterval( () => {
-            this.loaded = true;
+            this.loaded = true; // Sets loaded to true so getDom can create elements
             this.displayAlerts();
             this.updateDom(this.config.animationSpeed);
         }, this.config.displayInterval + this.config.animationSpeed);
@@ -78,10 +81,15 @@ Module.register('MMM-CanadianPublicWeatherAlerts', {
             this.updateDom();
         } else if (notification === "UPDATE") {
             this.currentAlerts = [];
+            // If notification payload contains alerts
             if (payload.length !== 0) {
                 this.currentAlerts = payload;
                 this.startDisplayTimer();
             } else {
+                // Clear displayed alerts and display console message
+                this.AlertTitle = "";
+                this.AlertRegion = "";
+                this.AlertTime = "";
                 Log.log(`[${this.name}] No Alerts in effect for configured regions`);
             }
 
